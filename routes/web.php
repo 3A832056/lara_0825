b4a160ec0f633a9dfa90d2fc48b6c340a6e8e95e
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-11-02 02:29:26 +0800

    設定 route 及對應的 Controller 回傳對應的 view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
+Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
+Route::get('contact', [\App\Http\Controllers\PostsController::class, 'contact'])->name('posts.contact');
+Route::get('about', [\App\Http\Controllers\PostsController::class, 'about'])->name('posts.about');
