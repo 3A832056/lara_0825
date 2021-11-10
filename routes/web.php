50c66b559f3d416cb0971727fcff5a9ee31319b2
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-11-10 11:50:02 +0800

    練習 5：刪除資料 使用 delete方法

--- routes/web.php
+++ routes/web.php
@@ -40,10 +40,13 @@ Route::get('/', function () {
 //        'content'=>'updated content',
 //    ]);
 
+//    $post=Post::find(1);
+//    $post->title='saved title';
+//    $post->content='saved content';
+//    $post->save();
+
     $post=Post::find(1);
-    $post->title='saved title';
-    $post->content='saved content';
-    $post->save();
+    $post->delete();
 });
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
 Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
