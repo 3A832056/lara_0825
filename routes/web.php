e3d0177a404714d1642cce9d4dd0937dc5b325bd
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-11-10 11:16:51 +0800

    用 create 的方法

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    $post = new Post();
-    $post->title ='test title';
-    $post->content='test content';
-    $post->save();
+    //$post = new Post();
+    //$post->title ='test title';
+    //$post->content='test content';
+    //$post->save();
 
+    Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+    ]);
 });
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
 Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
